7c054aab70a86e8d915fe124eac3b34dca89b386
Slothsoft <admin@slothsoft.de>
2019-09-08 12:18:14 +0200

    used the right equal() method now

--- test/exercise/blueprint-test.js
+++ test/exercise/blueprint-test.js
@@ -3,6 +3,6 @@ var functionUnderTest = require('../../src/exercise/blueprint.js');
 QUnit.module("blueprint", function() {
 	QUnit.test("hello world", function(assert) {
 		var result = functionUnderTest("World");
-		assert.deepEqual(result, "Hello World!");
+		assert.equal(result, "Hello World!");
 	});
 });
--- test/exercise/pig-latin-test.js
+++ test/exercise/pig-latin-test.js
@@ -3,34 +3,34 @@ var functionUnderTest = require('../../src/exercise/pig-latin.js');
 QUnit.module("pig-latin", function() {
 	QUnit.test("Brown Fox", function(assert) {
 		var result = functionUnderTest("The quick brown fox");
-		assert.deepEqual(result, "Hetay uickqay rownbay oxfay");
+		assert.equal(result, "Hetay uickqay rownbay oxfay");
 	});
 	QUnit.test("empty", function(assert) {
 		var result = functionUnderTest("");
-		assert.deepEqual(result, "");
+		assert.equal(result, "");
 	});
 	QUnit.test("null", function(assert) {
 		var result = functionUnderTest(null);
-		assert.deepEqual(result, null);
+		assert.equal(result, null);
 	});
 	QUnit.test("single word", function(assert) {
 		var result = functionUnderTest("house");
-		assert.deepEqual(result, "ousehay");
+		assert.equal(result, "ousehay");
 	});
 	QUnit.test("single word with captial letter", function(assert) {
 		var result = functionUnderTest("House");
-		assert.deepEqual(result, "Ousehay");
+		assert.equal(result, "Ousehay");
 	});
 	QUnit.test("single word with one letter", function(assert) {
 		var result = functionUnderTest("a");
-		assert.deepEqual(result, "aay");
+		assert.equal(result, "aay");
 	});
 	QUnit.test("single word with one captial letter", function(assert) {
 		var result = functionUnderTest("I");
-		assert.deepEqual(result, "Iay");
+		assert.equal(result, "Iay");
 	});
 	QUnit.test("many whitespace much wow", function(assert) {
 		var result = functionUnderTest("The    quick brown \t \n fox");
-		assert.deepEqual(result, "Hetay uickqay rownbay oxfay");
+		assert.equal(result, "Hetay uickqay rownbay oxfay");
 	});
 });
